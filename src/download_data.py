edbe88d891caf014f034fa919e5c5252e51f3581
Katie Birchard <33553358+katieb1@users.noreply.github.com>
2021-10-05 14:53:49 -0700

    First stab at package.xml, updated download script

--- src/download_data.py
+++ src/download_data.py
@@ -46,13 +46,8 @@ with tarfile.open(data, "r:gz") as tar:
     tar.extractall(path=transfer_dir)
     extracted_imgs = os.listdir(transfer_dir)
     for count, img in enumerate(extracted_imgs):
-        with rasterio.open(os.path.join(transfer_dir, img), "r") as raster:
-            values = raster.read()
-            if values.shape[0] == 1:
-                X.append(values[0])
-            else:
-                X.append(values)
-        y.append(target.target.iloc[0])
+        X.append(os.path.join(transfer_dir, img))
+        y.append(target.target.iloc[count])
         
 # Save X and y to an intermediate datasheet
                 
